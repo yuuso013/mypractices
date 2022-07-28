06196505b7ab48900219da6aa3e78b28e1de6a9c
Josuuuuu <josuevilera.epet20@gmail.com>
2022-07-28 13:46:53 -0300

    login completo con auth0

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./App.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile"
+import { useAuth0 } from '@auth0/auth0-react'
+
+
+function App() {
+
+    const {isAuthenticated, isLoading} = useAuth0()
+    
+    if (isLoading) return <h1>Loading...</h1>
+
+    return (
+        <div className="App">
+            <h1>Application</h1>
+            {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+            
+            <Profile/>
+        </div>
+    );
+}
+
+export default App;
