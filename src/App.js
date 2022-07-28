7f2fc5c1a772930323f363da63564cc014d69a22
Josuuuuu <josuevilera.epet20@gmail.com>
2022-07-28 15:12:06 -0300

    login-with-auth0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./App.css";
+import './App.css';
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import Profile from "./components/Profile";
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { useAuth0 } from '@auth0/auth0-react';
 import JSONPretty from 'react-json-pretty';
 import 'react-json-pretty/themes/monikai.css';
