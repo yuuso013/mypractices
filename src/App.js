b2c5b23a36a18cfe7ea41fb5c07772c9dc1e858c
Josué <82322641+yuuso013@users.noreply.github.com>
2022-08-03 16:10:09 -0300

    Revert "Josue"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 import "./App.css";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
-import Profile from "./components/Profile";
-import { useAuth0 } from "@auth0/auth0-react";
+import Profile from "./components/Profile"
+import { useAuth0 } from '@auth0/auth0-react'
 
 
 function App() {
