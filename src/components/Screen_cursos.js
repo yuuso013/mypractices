9469f73cb6c36b8de3d216cc10979dbceb1394f2
julian godoy <juliangodoy.epet20@gmail.com>
2022-08-04 17:01:59 -0300

    Boton de quinto

--- src/components/Screen_cursos.js
+++ src/components/Screen_cursos.js
@@ -5,6 +5,7 @@ export default function Screen_cursos(){
     return(
         <View>
             <Button title="E.P.E.T.20" onPress={() => console.log("Redireccionado")}></Button>
+            <Button title="5°" onPress={() => console.log("Quinto")}></Button>
         </View>
     );
 }
