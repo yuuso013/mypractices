caf90cc14013edd013c1603fa8892436f2c7de78
julian godoy <juliangodoy.epet20@gmail.com>
2022-08-04 16:56:52 -0300

    empezando con el return de la funcion

--- src/components/Screen_cursos.js
+++ src/components/Screen_cursos.js
@@ -2,5 +2,9 @@ import React from "react";
 import { Button, Text, View } from "react-native";
 
 export default function Screen_cursos(){
-    
+    return(
+        <View>
+            
+        </View>
+    );
 }
