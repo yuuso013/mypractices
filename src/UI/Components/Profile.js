7f850093017f4883064959afe453bf957e14bf47
juliangodoy.epet20@gmail.com <juliangodoy.epet20@gmail.com>
2022-09-13 04:31:34 -0300

    haciendo avance sobre las imagenes de la app

--- src/UI/Components/Profile.js
+++ src/UI/Components/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-
+import epet20 from "../Styles/img/epet20.png";
 
 export const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -11,10 +11,28 @@ export const Profile = () => {
 
   return (
     isAuthenticated && (
-      <div className="App-profile"> 
-        <h2>¡Hola {user.name}! ¡Bienvenido/a a Mis Prácticas!</h2>
-        <h2>Email del usuario: {user.email}</h2>
+      <div className="App-profile">
+        <table>
+          <tr>
+            <td>
+              <div className="imgepet1">
+                <img src={epet20} alt='Epet20_imagen' />
+              </div>
+            </td>
+            <td>
+              <h2>¡Hola {user.name}! ¡Bienvenido/a a Mis Prácticas!</h2>
+              <h2>Email del usuario: {user.email}</h2>
+            </td>
+            <td>
+              <div className="imgepet2">
+                <img src={epet20} alt='Epet20_imagen' />
+              </div>
+            </td>
+          </tr>
+        </table >
       </div>
+
+
 
     )
   );
