41b0a1ecfbe6e2e9ca0d63cf44d3db9991da0fb2
juliangodoy.epet20@gmail.com <juliangodoy.epet20@gmail.com>
2022-09-13 03:38:49 -0300

    añadiendo boton de casa de inicio a SectionTwo6

--- src/UI/Components/Sections/SectionTwo6.js
+++ src/UI/Components/Sections/SectionTwo6.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth0 } from "@auth0/auth0-react";
-
+import imgcasa from "../../Styles/img/imgcasa.png"
 
 export const SectionTwo6 = () => {
   const { isAuthenticated, isLoading } = useAuth0();
@@ -11,15 +11,17 @@ export const SectionTwo6 = () => {
   }
   return (
     isAuthenticated && (
-    <div className='Sections'>
+      <div className='Sections'>
         <h2>6° Año | 2do curso</h2>
 
         <button type="submit" className="Button">
           <Link to="/Section" className="Link">
-            <p>Inicio</p>
+            <div className='imageninicio'>
+              <img src={imgcasa} alt='Inicio' />
+            </div>
           </Link>
         </button><br /><br />
-    </div>
+      </div>
     )
   )
 }
