bc92efa2041c1b774b0a6aa358d7dac19278be12
juliangodoy.epet20@gmail.com <juliangodoy.epet20@gmail.com>
2022-09-13 03:41:13 -0300

    añadiendo boton de casa de inicio a SectionThree5

--- src/UI/Components/Sections/SectionThree5.js
+++ src/UI/Components/Sections/SectionThree5.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth0 } from "@auth0/auth0-react";
+import imgcasa from "../../Styles/img/imgcasa.png"
 
 export const SectionThree5 = () => {
   const { isAuthenticated, isLoading } = useAuth0();
@@ -10,15 +11,17 @@ export const SectionThree5 = () => {
   }
   return (
     isAuthenticated && (
-    <div className='Sections'>
+      <div className='Sections'>
         <h2>5° Año | 3er curso</h2>
-    
+
         <button type="submit" className="Button">
           <Link to="/Section" className="Link">
-            <p>Inicio</p>
+            <div className='imageninicio'>
+              <img src={imgcasa} alt='Inicio' />
+            </div>
           </Link>
         </button><br /><br />
-    </div>
+      </div>
     )
   )
 }
