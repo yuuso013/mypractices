06b4b47fc81b289fb93a0c875dbb5f910485ca67
Josuuuuu <josuevilera.epet20@gmail.com>
2022-11-21 22:14:28 -0300

    cambio img agregada

--- src/UI/Components/Login.js
+++ src/UI/Components/Login.js
@@ -11,7 +11,13 @@ export const LoginButton = () => {
   return (
     <div className="App-header">
       <h1>Si desea ingresar a "Mis Prácticas" para observar su lugar de pasantía, inicie sesión</h1>
+      <td className="td_profile">
+              <div className="imgprofile2">
+                <img src={logotipo_app} alt='imagenLogotipo' />
+              </div>
+            </td>
       <button className="Button" onClick={() => loginWithRedirect()}>Iniciar sesión</button>
     </div>
+    
   )  
 }; 
